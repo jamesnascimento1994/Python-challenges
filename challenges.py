ef1547f9ad8e1bea408d21ab53e8d77459819f90
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-01 22:18:19 -0500

    solution for N-1 converter

--- challenges.py
+++ challenges.py
@@ -1,14 +1,31 @@
 # Challenge One: This code does not execute properly. Try to figure out why.
-# Challenge Source:
+# Challenge Source: https://www.codewars.com/kata/50654ddff44f800200000004/python
 
 # Starter Code
 # def multiply(a, b):
 #     a * b
 
 # Solution
-def multiply(a, b):
-    return a * b
+# def multiply(a, b):
+#     return a * b
 
-print(multiply(7, 8));
-print(multiply(4, 3));
-print(multiply(10, 5));
+# # TESTS
+# print(multiply(7, 8));
+# print(multiply(4, 3));
+# print(multiply(10, 5));
+
+# Challenge Two: We want an array, but not just any old array, an array with contents! Write a function that produces an array with the numbers 0 to N-1 in it.
+# Challenge Source: https://www.codewars.com/kata/571d42206414b103dc0006a1/python
+
+# def arr(n=None): 
+#     new_array = []
+#     if n == None:
+#         return []
+#     for x in range(n):
+#         new_array.append(x)
+#     return new_array
+
+# # TESTS
+# print(arr(5));
+# print(arr(7));
+# print(arr(8));
