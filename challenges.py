e49454945fa9bce087c671c4d4179273c28bce5d
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-01 22:11:26 -0500

    solution for buggy code with missing return statement

---
+++ challenges.py
@@ -0,0 +1,14 @@
+# Challenge One: This code does not execute properly. Try to figure out why.
+# Challenge Source:
+
+# Starter Code
+# def multiply(a, b):
+#     a * b
+
+# Solution
+def multiply(a, b):
+    return a * b
+
+print(multiply(7, 8));
+print(multiply(4, 3));
+print(multiply(10, 5));
