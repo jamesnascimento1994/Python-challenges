240da3eeab7ef4bf7e6a76ee63bb180e1feedb16
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-03-01 22:35:14 -0500

    solution for vowel remover filter function

--- challenges.py
+++ challenges.py
@@ -42,3 +42,21 @@
 # print(song_decoder('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB'))
 # print(song_decoder("WUBFORGIVEWUBMEWUBFATHERWUB"))
 # print(song_decoder("WUBJESSEWUBBADWUBBOYWUBJUSTWUBCOMEWUBLOOKWUBATWUBWHATWUBYOURWUBBROTHERWUBDIDWUB"))
+
+# Challenge Four: Trolls are attacking your comment section!
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+# Note: for this kata y isn't considered a vowel.
+# Challenge Source: https://www.codewars.com/kata/52fba66badcd10859f00097e/python
+
+def disemvowel(string):
+    output = []
+    for char in string:
+        if char not in "aeiouAEIOU":
+            output.extend(char)
+    return "".join(output)
+
+# TESTS
+print(disemvowel("This website is for losers! LOL"))
+print(disemvowel("Do you like biscuits?"))
