69e8735cb6a51dddd87266bdae6ae3739b9e40f4
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-12-24 13:57:49 -0500

    solution for descending order

--- challenges.py
+++ challenges.py
@@ -159,23 +159,35 @@
 # Challenge Source: https://www.codewars.com/kata/5412509bd436bd33920011bc
 
 # return masked string
-def maskify(cc):
-    if len(cc) > 4:
-        reversedCC = cc[::-1]
-        newString = ''
-        for i in range(0, len(reversedCC)):
-            if i < 4:
-                newString += reversedCC[i]
-            else:
-                newString += '#'
-        return newString[::-1]
-    else:
-        return cc;
+# def maskify(cc):
+#     if len(cc) > 4:
+#         reversedCC = cc[::-1]
+#         newString = ''
+#         for i in range(0, len(reversedCC)):
+#             if i < 4:
+#                 newString += reversedCC[i]
+#             else:
+#                 newString += '#'
+#         return newString[::-1]
+#     else:
+#         return cc;
+
+# # TESTS
+# print(maskify("4556364607935616"))
+# print(maskify("64607935616"))
+# print(maskify("1"))
+# print(maskify(""))
+# print(maskify("Skippy"))
+# print(maskify("Nananananananananananananananana Batman!"))
+
+# Challenge Eleven: Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+# Challenge Source: https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+def descending_order(num):
+    # Bust a move right here
+    return int("".join(map(str, sorted([i for i in str(num)], reverse=True))));
 
 # TESTS
-print(maskify("4556364607935616"))
-print(maskify("64607935616"))
-print(maskify("1"))
-print(maskify(""))
-print(maskify("Skippy"))
-print(maskify("Nananananananananananananananana Batman!"))
+print(descending_order(0));
+print(descending_order(15));
+print(descending_order(123456789));
