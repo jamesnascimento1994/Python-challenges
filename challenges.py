0dded92f5466851a6dc1f1a17d045aef1a30001b
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-12-25 22:54:36 -0500

    List filtering solution

--- challenges.py
+++ challenges.py
@@ -183,11 +183,26 @@
 # Challenge Eleven: Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 # Challenge Source: https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 
-def descending_order(num):
-    # Bust a move right here
-    return int("".join(map(str, sorted([i for i in str(num)], reverse=True))));
+# def descending_order(num):
+#     # Bust a move right here
+#     return int("".join(map(str, sorted([i for i in str(num)], reverse=True))));
 
-# TESTS
-print(descending_order(0));
-print(descending_order(15));
-print(descending_order(123456789));
+# # TESTS
+# print(descending_order(0));
+# print(descending_order(15));
+# print(descending_order(123456789));
+
+# Challenge Twelve: In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+# Challenge Source: https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+
+def filter_list(l):
+    new_list = []
+    for x in l:
+        if type(x) == int:
+            new_list.append(x)
+    
+    return new_list
+
+print(filter_list([1,2,'a','b']))
+print(filter_list([1,'a','b',0,15]))
+print(filter_list([1,2,'aasf','1','123',123]))
