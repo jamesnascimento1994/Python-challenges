e53826959e1952d66d4950b1b4606d84750eb148
jamesnascimento1994 <james.nascimento94@yahoo.com>
2021-12-30 19:47:51 -0500

    solution for pagination helper

--- challenges.py
+++ challenges.py
@@ -297,19 +297,76 @@
 
 # Challenge Source: https://www.codewars.com/kata/520b9d2ad5c005041100000f
 
-def pig_it(text):
-    #your code here
-    words = text.split(' ')
-    pig_sentence = ' '
+# def pig_it(text):
+#     #your code here
+#     words = text.split(' ')
+#     pig_sentence = ' '
     
-    for word in words:
-        if word in '!.%&?':
-            pig_sentence += word
+#     for word in words:
+#         if word in '!.%&?':
+#             pig_sentence += word
+#         else:
+#             pig_word = word[1:] + word[0] + 'ay'
+#             pig_sentence += pig_word + ' '
+#     return pig_sentence.strip()
+
+# # TESTS
+# print(pig_it('Pig latin is cool'))
+# print(pig_it('Hello world !'))
+
+# Challenge Seventeen: For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array. The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+# Challenge Source: https://www.codewars.com/kata/515bb423de843ea99400000a
+
+import math
+class PaginationHelper:
+
+  # The constructor takes in an array of items and a integer indicating
+  # how many items fit within a single page
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+      
+  
+  # returns the number of items within the entire collection
+    def item_count(self):
+        return len(self.collection)
+     
+  # returns the number of pages
+    def page_count(self):
+        return int(math.ceil(1.0 * self.item_count() / self.items_per_page))
+      
+    
+  # returns the number of items on the current page. page_index is zero based
+  # this method should return -1 for page_index values that are out of range
+    def page_item_count(self,page_index):
+        items_per_page = self.items_per_page
+        if page_index >= self.page_count():
+            return -1
+        elif page_index == self.page_count() - 1:
+            return self.item_count() - self.items_per_page*page_index
         else:
-            pig_word = word[1:] + word[0] + 'ay'
-            pig_sentence += pig_word + ' '
-    return pig_sentence.strip()
+            return self.items_per_page
+      
+  
+  # determines what page an item is on. Zero based indexes.
+  # this method should return -1 for item_index values that are out of range
+    def page_index(self,item_index):
+        if item_index in range(self.item_count()):
+            return item_index//self.items_per_page
+        
+        return -1
 
 # TESTS
-print(pig_it('Pig latin is cool'))
-print(pig_it('Hello world !'))
+collection = range(1, 25)
+collection = ['a','b','c','d','e','f']
+helper = PaginationHelper(collection, 4)
+print(helper.page_count())
+print(helper.item_count())
+print(helper.page_item_count(0))
+print(helper.page_item_count(1))
+print(helper.page_item_count(2))
+print(helper.page_index(5))
+print(helper.page_index(2))
+print(helper.page_index(20))
+print(helper.page_index(-1))
