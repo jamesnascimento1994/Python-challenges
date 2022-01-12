de2f0968e4855c41d79f43713f20ff8966b50042
jamesnascimento1994 <james.nascimento94@yahoo.com>
2022-01-11 22:08:39 -0500

    solution for pick peaks

--- challenges.py
+++ challenges.py
@@ -402,31 +402,70 @@
 
 # Challenge Source: https://www.codewars.com/kata/5270d0d18625160ada0000e4
 
-def score(dice):
-    # your code here
-    from collections import Counter
-    points = {
-        1: 1000,
-        6: 600,
-        5: 500,
-        4: 400,
-        3: 300,
-        2: 200
-    }
-    dices = Counter(dice)
+# def score(dice):
+#     # your code here
+#     from collections import Counter
+#     points = {
+#         1: 1000,
+#         6: 600,
+#         5: 500,
+#         4: 400,
+#         3: 300,
+#         2: 200
+#     }
+#     dices = Counter(dice)
     
-    total = 0
+#     total = 0
     
-    for key,val in dices.items():
-        if val >= 3:
-            total += points[key] * (val // 3)
-        if key == 1:
-            total += 100 * (val % 3)
-        elif key == 5:
-            total += 50 * (val % 3)
+#     for key,val in dices.items():
+#         if val >= 3:
+#             total += points[key] * (val // 3)
+#         if key == 1:
+#             total += 100 * (val % 3)
+#         elif key == 5:
+#             total += 50 * (val % 3)
             
-    return total
+#     return total
 
-print(score([5, 1, 3, 4, 1]))
-print(score([1, 1, 1, 3, 1]))
-print(score([2, 4, 4, 5, 4]))
+# # TESTS
+# print(score([5, 1, 3, 4, 1]))
+# print(score([1, 1, 1, 3, 1]))
+# print(score([2, 4, 4, 5, 4]))
+
+# Challenge Twenty: In this kata, you will write a function that returns the positions and the values of the "peaks" (or local maxima) of a numeric array. For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with a value of 5 (since arr[3] equals 5). The output will be returned as an object with two properties: pos and peaks. Both of these properties should be arrays. If there is no peak in the given array, then the output should be {pos: [], peaks: []}. Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should return {pos: [3, 7], peaks: [6, 3]} (or equivalent in other languages). All input arrays will be valid integer arrays (although it could still be empty), so you won't need to validate the input. The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is after and before and therefore, we don't know if it is a peak or not). Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] and [1, 2, 2, 2, 2] do not. In case of a plateau-peak, please only return the position and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent in other languages)
+
+# Challenge Source: https://www.codewars.com/kata/5279f6fe5ab7f447890006a7
+
+def pick_peaks(arr):
+    #your code here
+    output = {"pos":[], "peaks": []}
+    
+    if len(arr) == 0: return output
+    top_peak = arr[0]
+    top_pos = 0
+
+    for i in range(0, len(arr)):
+        if arr[i] > arr[i-1]:
+            top_peak = arr[i]
+            top_pos = i;
+        elif arr[i] == arr[i-1]:
+            print("Plateau ", arr[i-1], " to ", arr[i])
+        elif arr[i] < arr[i-1]:
+            if top_pos > 0:
+                output["pos"].append(top_pos)
+                output["peaks"].append(top_peak)
+                top_pos = 0
+            
+    return output
+
+# TESTS
+print(pick_peaks([1,2,3,6,4,1,2,3,2,1]))
+print(pick_peaks([3,2,3,6,4,1,2,3,2,1,2,3]))
+print(pick_peaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]))
+print(pick_peaks([2,1,3,1,2,2,2,2,1]))
+print(pick_peaks([2,1,3,1,2,2,2,2]))
+print(pick_peaks([2,1,3,2,2,2,2,5,6]))
+print(pick_peaks([2,1,3,2,2,2,2,1]))
+print(pick_peaks([1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3]))
+print(pick_peaks([]))
+print(pick_peaks([1,1,1,1]))
