80c275dac6974f2b40d33f942a3db0651b5ea595
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-05-02 17:28:54 -0400

    added solution for doors

--- challenges.py
+++ challenges.py
@@ -571,8 +571,9 @@ def valid_braces(braces):
 # print(valid_braces("[(])"))
 # print(valid_braces("[({})](]"))
 
-# Challenge Twenty-Five:
-# Challenge Source:
+# Challenge Twenty-Five: Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e. You need to return the nth triangular number. You should return 0 for out of range values
+
+# Challenge Source: https://www.codewars.com/kata/525e5a1cb735154b320002c8/python
 
 def triangular(n):
     if n < 1:
@@ -581,7 +582,21 @@ def triangular(n):
         return int(n * (n + 1) // 2)
 
 # TESTS
-print(triangular(0))
-print(triangular(2))
-print(triangular(3))
-print(triangular(-10))
+# print(triangular(0))
+# print(triangular(2))
+# print(triangular(3))
+# print(triangular(-10))
+
+# Challenge Twenty-Six: In the morning all the doors in the school are closed. The school is quite big: there are N doors. Then pupils start coming. It might be hard to believe, but all of them want to study! Also, there are exactly N children studying in this school, and they come one by one. When these strange children pass by some doors they change their status (i.e. Open -> Closed, Closed -> Open). Each student has their number, and each i-th student alters the status of every i-th door. For example: when the first child comes to the schools, he changes every first door (he opens all of them). The second one changes the status of every second door (he closes some doors: the 2nd, the 4th and so on). Finally, when the last one – the n-th – comes to the school, he changes the status of each n-th door (there's only one such door, though).You need to count how many doors are left opened after all the students have come.
+
+# Challenge Source: https://www.codewars.com/kata/57c15d314677bb2bd4000017/python
+
+def doors(n):
+    return math.floor(math.sqrt(n))
+
+# TESTS
+print(doors(0))
+print(doors(2))
+print(doors(3))
+print(doors(5))
+print(doors(10))
